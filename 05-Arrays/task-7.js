b8153f6e0d8b104ebe015fb0389444f22b4ca960
georgemff <g_adeishvili2@cu.edu.ge>
2019-05-31 19:12:33 +0400

    fix(05-Arrays): Fix task-7

--- 05-Arrays/task-7.js
+++ 05-Arrays/task-7.js
@@ -1,6 +1,6 @@
 const arr = ['abcd', 'abcde', 'ab', 'abc'];
 
 let arrLength = arr.map((item, i, arr) => {
-    return item.length + ' - ' + item;
+    return item.length;
 });
 console.log('Array Length with Array item: ' + arrLength);
