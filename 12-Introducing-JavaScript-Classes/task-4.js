6ad6547f0760899c25ab829fed0e364db0bf5d9e
georgemff <g_adeishvili2@cu.edu.ge>
2019-06-10 19:01:00 +0400

    feat(12-Introduction-Javascript-Classes): Finish tasks

---
+++ 12-Introducing-JavaScript-Classes/task-4.js
@@ -0,0 +1,29 @@
+class MyString {
+        reverse = (string) => {
+            let stringArray = string.split('');
+            let reverseArray = stringArray.reverse();
+            let joinArray = reverseArray.join('');
+            return joinArray;
+        };
+
+        ucFirst = (string) => {
+            let upperCase = string.charAt(0).toUpperCase();
+            return upperCase + string.slice(1);
+        };
+
+        ucWords = (string) => {
+            let words = string.split(' ');
+            let upperArray = [];
+            for(let word of words) {
+                let upperCase = word.charAt(0).toUpperCase();
+                upperArray.push(upperCase + word.slice(1));
+            }
+            return upperArray.join(' ');
+        }
+}
+
+var str = new MyString();
+
+console.log(str.reverse('abcde')); // print 'edcba'
+console.log(str.ucFirst('abcde')); // print 'Abcde'
+console.log(str.ucWords('abcde abcde abcde')); // print 'Abcde Abcde Abcde'
---
+++ 12-Introducing-JavaScript-Classes/task-5.js
@@ -0,0 +1,25 @@
+class Validator {
+    isEmail = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    };
+
+    isDate = (date) => {
+        return !isNaN(Date.parse(date));
+    };
+
+    isDomain = (domain) => {
+        let format = /[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
+        return format.test(String(domain).toLowerCase());
+    };
+
+    isPhone = (phone) => {
+        let format = /^\+?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{2})?[-. ]?([0-9]{2})?[-. ]?([0-9]{2})$/;
+        return format.test(String(phone).toLowerCase());
+    };
+}
+
+let validator = new Validator();
+console.log(validator.isEmail('jshtml@gmail.ru'));
+console.log(validator.isDomain('jsshtml.net'));
+console.log(validator.isDate('12.05.2020'));
+console.log(validator.isPhone('+995-599-45-18-25')); // it can be format of your country
