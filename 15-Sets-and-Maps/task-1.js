eb166dd94119c691aa6234999bd2c5cacd19d659
georgemff <g_adeishvili2@cu.edu.ge>
2019-06-14 00:41:26 +0400

    fix(15-Sets-and-Maps): Fix task

--- 15-Sets-and-Maps/task-1.js
+++ 15-Sets-and-Maps/task-1.js
@@ -1,16 +1,31 @@
 class DB  {
     constructor() {
     this.user = new Map();
+    this.userid = 0;
 };
-     create(person){
+    create(person){
         if(typeof person !== 'object') {
             throw new Error('parameter must be an Object!');
         }
         if(!person.name || !person.age || !person.country || !person.salary){
             throw new Error('Field is required');
         }
-        let id = 'userID: ' + this.user.size;
+        if(typeof person.name !== 'string'){
+            throw new Error('Name must be a string');
+        }
+        if(typeof person.age !== 'number'){
+            throw new Error('Age must be a number');
+        }
+         if(typeof person.country !== 'string'){
+             throw new Error('Country must be a number');
+         }
+         if(typeof person.salary !== 'number'){
+             throw new Error('Salary must be a number');
+         }
+        let id = 'userID: ' + this.userid;
+         this.userid++;
         this.user.set(id,person);
+        this.user.get(id).id = id;
         return id;
     }
 
@@ -21,10 +36,10 @@ class DB  {
         if(arguments.length === 0){
             throw new Error('Parameter is Empty!');
         }
-        if(this.user.has(id))
-        {
+        if(!this.user.has(id)) {
+            return null;
+        }
             return this.user.get(id);
-        } else return null;
     }
 
     readAll(){
@@ -32,7 +47,7 @@ class DB  {
             throw new Error('Unexpected Parameters!');
         }
         let arr = [];
-        arr.push(...this.user.entries());
+        arr.push(...this.user.values());
         return arr;
     }
 
@@ -82,9 +97,17 @@ const person = {
     country: 'ge', // required field with type string
     salary: 500 // required field with type number
 };
+const person1 = {
+    name: 'joe', // required field with type string
+    age: 23, // required field with type number
+    country: 'eu', // required field with type string
+    salary: 800 // required field with type number
+};
 
 const id = db.create(person);
+const id1 = db.create(person1);
 const customer = db.read(id);
+console.log(customer);
 const customers = db.readAll(); // array of users
 console.log(customers);
 console.log(db.update(id, { age: 22 })); // id
